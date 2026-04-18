0ffa52eae084a06450edb4aebfb27a7eb43188d9
Mis-hero <12423@holbertonstudents.com>
2026-04-18 23:30:33 +0300

    4

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""This module defines typed variables with basic Python annotations."""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
