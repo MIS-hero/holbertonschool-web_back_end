e7a7b54f8bfbec235b9f740123b7d509ca12e0bb
Mis-hero <12423@holbertonstudents.com>
2026-04-18 23:26:41 +0300

    Add concat function to concatenate two strings with detailed docstring

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A module provides a function that concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings.
+
+    Args:
+        str1(str): first string
+        str2(str): second string
+
+    Returns:
+        str: concatenation of the two strings
+    """
+    return str1 + str2
