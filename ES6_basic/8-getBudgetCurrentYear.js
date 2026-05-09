0c8be0d1bb27ea350917379b634eb7c29e55182f
Mis-hero <12423@holbertonstudents.com>
2026-05-09 20:48:14 +0300

    8

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,7 @@
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+}
